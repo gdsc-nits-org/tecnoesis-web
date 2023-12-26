0386fcc277dc25e73fe3b2cdd754d1bcf4f58b73
Mohit Agarwal <mohitagarwal.1031@gmail.com>
2023-12-26 17:40:54 +0530

    Responsive for mobile and tablet view

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import styles from '../Sponsors/Sponsors.module.css';
-
 import Cards from '../../assets/Card.json';
 
 const Sponsors = () => {
@@ -10,18 +8,19 @@ const Sponsors = () => {
       <img
         className={styles.bg_container}
         src="images/bg_landing_stars.svg"
+        alt="Background stars"
       />
       <img className={styles.text_container} src="images/sponsor_logo.png" />
 
       {Cards &&
         Cards.map((item) => (
-          <div key={item.id}>
-            <img className={styles.mn_fr_container} src="images/frame.png" />
+          <div key={item.id} className={styles.mn_fr_container}> {/* Apply mn_fr_container to the parent div */}
+            <img src="images/frame.png" />
             <img className={styles.fr_container} src={item.company} />
           </div>
         ))}
     </section>
   );
-}
+};
 
-export default Sponsors
+export default Sponsors;
