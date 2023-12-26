26b43966917e7c15f852d3ee424a644af1106b8a
Rupantar <rupantarborphukan@gmail.com>
2023-12-26 13:32:57 +0530

    update sponsor section

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -2,27 +2,25 @@ import React from 'react';
 
 import styles from '../Sponsors/Sponsors.module.css';
 
-import StarBg from "../../assets/Images/bg_landing_stars.svg";
-import Sponsor from '../../assets/Images/sponsor_logo.png';
-import frame from '../../assets/Images/frame.png';
-
-
 import Cards from '../../assets/Card.json';
 
 const Sponsors = () => {
   return (
-    <div>
-      <img className={styles.main_container} src={StarBg} />
-      <img className={styles.text_container} src={Sponsor} />
+    <section className={styles.main_container}>
+      <img
+        className={styles.bg_container}
+        src="images/bg_landing_stars.svg"
+      />
+      <img className={styles.text_container} src="images/sponsor_logo.png" />
 
       {Cards &&
         Cards.map((item) => (
           <div key={item.id}>
-            <img className={styles.mn_fr_container} src={frame} />
+            <img className={styles.mn_fr_container} src="images/frame.png" />
             <img className={styles.fr_container} src={item.company} />
           </div>
         ))}
-    </div>
+    </section>
   );
 }
 
