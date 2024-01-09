af921fd5463253ac3d38d4ac40f997ad2f88549b
Swarup Chanda <119785118+swarupecenits@users.noreply.github.com>
2024-01-09 18:44:58 +0530

    Update Button_page.jsx

--- src/components/Button_page/Button_page.jsx
+++ src/components/Button_page/Button_page.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './Button_page.module.css'; 
 
-const Button_page = ({ onClick, children }) => {
+const Button_page = ({comp, onClick, children }) => {
   return (
-    <button className={styles.footer_button} onClick={onClick}>
+    <button className={styles.footer_button} style={{borderRadius:`${comp==='home'?'9999px':'0px'}`}} onClick={onClick}>
       <div className={styles.footer_button_style}>
         {children}
       </div>
