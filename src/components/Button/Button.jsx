f70a046f3cd6b65693b1952735f1d351fa37d087
Swarup Chanda <swarupchanda1963@gmail.com>
2024-01-11 15:23:11 +0530

    Made rounded boolean type and remove the comment

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './Button.module.css'; 
 
 const Button = ({rounded, onClick, children }) => {
-  return (// Setting the border radius based on the value of the comp prop
-    <button className={styles.footer_button} style={{borderRadius:`${rounded==='round'?'9999px':'0px'}`}} onClick={onClick}>
+  return (
+    <button className={styles.footer_button} style={{borderRadius: `${rounded ? '9999px' : '0px'}`}} onClick={onClick}>
       <div className={styles.footer_button_style}>
         {children}
       </div>
