13626b30d28ef1893b0f84af08eea188b10aaad6
ZinalPotphode <zinalvijaypotphode@gmail.com>
2024-01-18 14:34:19 +0530

    Add: added styles and link to backicon

--- src/pages/EventDescription/EventDescription.jsx
+++ src/pages/EventDescription/EventDescription.jsx
@@ -1,9 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import styles from "./EventDescription.module.css";
 import robowarsImage from "/images/robowars.png";
-import robologo_demo from "/images/robologo_demo.png";
 import backIcon from "/images/backIcon.png";
+import modulesData from "../../assets/modulesData.json";
+import { Link } from 'react-router-dom'; 
 
 const EventDescription = () => {
+  const [moduleData, setModuleData] = useState(null);
+
+  useEffect(() => {
+    
+    const selectedModule = modulesData.find((module) => module.moduleId === 1);
+
+    if (selectedModule) {
+      setModuleData(selectedModule);
+    }
+  }, [1]);
+
+  if (!moduleData) {
+    return null; 
+  }
+
+  const { moduleHeading, description, centerImage } = moduleData;
   return (
     <div className={styles.container}>
       
@@ -11,30 +29,23 @@ const EventDescription = () => {
       <div className={styles.gradient} >
       
       <div className={styles.robowars}>
-      <div className={styles.backIcon}><img  alt="" src={backIcon} /></div>
+      <Link to="">
+      <div className={styles.backIcon}><img  alt="" src={backIcon} /></div></Link>
          <div className={styles.robowars_img}> <img src={robowarsImage} alt="Robowars"></img></div>
         </div>
     
         <div className={styles.moduleWrapper}>
-          Module : Robotron
+          Module : {moduleHeading}
         </div>
         <div className={styles.center}>
         <div className={styles.description}>
-          <p
-            className={styles.text}
-          >Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-          <p className={styles.text}>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec Lorem ipsum dolor sit amet,
-            consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-            Aenean massa.
-          </p>
+        {description.map((paragraph, index) => (
+              <p key={index} className={styles.text}>{paragraph}</p>
+            ))}
         </div>
         <div className={styles.center_img}>
           
-            <img alt="robologo_demo.png" src={robologo_demo} />
+            <img alt="robologo_demo.png" src={centerImage} />
             
         
         </div>
