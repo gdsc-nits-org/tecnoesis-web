070864283db0a93cbb378ef7b47715226cc7f34e
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2024-01-19 21:05:25 +0530

    add firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyChMa5aDWDVUm08xYcLjjIh2em1L2X6P8c",
+  authDomain: "tecno-demo-e2579.firebaseapp.com",
+  projectId: "tecno-demo-e2579",
+  storageBucket: "tecno-demo-e2579.appspot.com",
+  messagingSenderId: "1090712369843",
+  appId: "1:1090712369843:web:70928e4388cce434238ec6",
+  measurementId: "G-W1N1KKH4JB"
 };
 
 
