2e0733d84c9c02d7d00f43552ea54ebf2ebfbea7
barnilsarma <barnilsarma@gmail.com>
2024-01-20 10:23:21 +0530

    api integration of event registration

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,6 +1,7 @@
 import styles from './Registration.module.css';
-import { useState, useRef } from 'react'
-const Card = (props) => {
+import { useState, useEffect, useRef } from 'react'
+
+const Card = ({ id, setMembers, name }) => {
     const [active, setActive] = useState(false);
     const toggleStatement = useRef(null);
     const toggleIcon = useRef(null);
@@ -8,59 +9,75 @@ const Card = (props) => {
     var u = window.getComputedStyle(t, null);
     var closed = u.getPropertyValue('--inactive');
     var opened = u.getPropertyValue('--active');
-    const detailsClick = (e) => {
-        e.preventDefault();
-        e.target.innerHTML = "EDIT";
-        setActive(active === false ? true : false);
+    // const detailsClick = (e) => {
+    //     e.preventDefault();
+    //     e.target.innerHTML = "EDIT";
+    //     setActive(active === false ? true : false);
 
+    // }
+    const handleChange = (e) => {
+        setMembers(prevMembers => {
+            const newMembers = [...prevMembers];
+            newMembers[id] = e.target.value;
+            return newMembers;
+        });
     }
     return (
         <div className={styles.parent}>
-            <div key={props.id} className={styles.memberBox}>
+            <div key={id} className={styles.memberBox}>
                 <div className={styles.leftPortion}>
-                    <h1 className={styles.memberName}>{props.username}</h1>
-                </div>
-                <div className={styles.toggler}>
-                    <button className={styles.addDetails} name={props.username} onClick={detailsClick} style={{ opacity: `${active === false ? '1' : '0'}`, transition: 'ease 500ms' }} ref={toggleStatement} >
-                        Add Details
-                    </button>
-                    <button className={styles.addDetailsIcon} style={{ background: 'transparent', border: 'none', transition: 'ease 500ms', opacity: `${active === false ? '0' : '1'}` }} onClick={detailsClick} ref={toggleIcon}>
-                        <img src='https://res.cloudinary.com/dhry5xscm/image/upload/v1705506833/tecnoesis/toggle_arrow_io69yk.svg' alt="toggle button..." />
-                    </button>
-                </div>
-            </div>
-            <div className={styles.toggleActiveState} style={{ opacity: `${active === false ? '0' : '1'}`, height: `${active === false ? closed : opened}`, transition: 'ease 500ms' }}>
-                <div className={styles.memberInfo}>
-                    <input type="text" placeholder='Enter your name' className={styles.memberInput} />
-                    <input type="text" placeholder='Enter your phone number' className={styles.memberInput} />
+                    <h1 className={styles.memberName}>USERNAME</h1>
+                    <div className={styles.memberInfo}>
+                        <input type="text" placeholder='Enter your name' value={name} onChange={handleChange} className={styles.memberInput} />
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
 const Registration = () => {
-    const [count, setCount] = useState(1);
-    var db = [
-        {
-            id: 1,
-            username: 'Member 1',
-            phone: 'xxxxxxxxxx',
-        }
-    ];
-    const [DB, setDB] = useState(db);
-    const addMember = (e) => {
+    const [maxMember, setmaxMember] = useState();
+    const [teamName, setTeamName] = useState("");
+    const [members, setMembers] = useState([]);
+    const [eventId, seteventId] = useState('65a5849d3232bd1424b3ef55');
+    async function submitForm(e) {
         e.preventDefault();
-        db = DB;
-        db.push(
-            {
-                id: count + 1,
-                username: `Member ${count + 1}`,
-                phone: 'xxxxxxxxxx'
-            }
-        );
-        setCount(count + 1);
-        setDB(db);
+        let filter = members.filter((item) => item != "");
+        // let response = await fetch("https://tecnoesis-api.onrender.com/api/team/event/65a5849d3232bd1424b3ef55/add", {
+        let response = await fetch("http://localhost:4000/api/team/event/65a5849d3232bd1424b3ef55/add", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer 1000000"
+            },
+            body: JSON.stringify({
+                "name": teamName,
+                "members": filter,
+                "extrainformation": "This team specialises in AI and machine learning projects"
+            })
+        });
+
+        if (response.status == 200) {
+            console.log(response.status)
+        }
     }
+    async function fetchData() {
+        // let response = await fetch("https://tecnoesis-api.onrender.com/api/event", { method: 'GET' });
+        let response = await fetch("http://localhost:4000/api/event", { method: 'GET' });
+        let maxNumber = await response.json();
+        let arr = maxNumber.msg;
+        let arrFilter = arr.filter((item) => item.id = eventId);
+        let msg = arrFilter[0].maxTeamSize;
+        setmaxMember(msg);
+        // console.log(msg);
+        let temp = Array.from({ length: msg - 1 }, () => "");
+        setMembers(temp);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
         <div className={styles.regCont}>
             <h1 className={styles.mainHeading}>REGISTRATION FORM</h1>
@@ -71,19 +88,20 @@ const Registration = () => {
                 <form className={styles.formCont}>
                     <div className={styles.teamNameCont}>
                         <h1 className={styles.teamName}>TEAM NAME</h1>
-                        <input type="text" className={styles.teamField} placeholder="Enter your team name here.." />
+                        <input type="text" className={styles.teamField} value={teamName} onChange={(e) => setTeamName(e.target.value)} placeholder="Enter your team name here.." />
                     </div>
                     <div className={styles.memberCont}>
                         {
-                            DB.map((item) =>
-                                <Card id={item.id} username={item.username} />
+                            members.map((member, index) =>
+                                <Card key={index} id={index} name={member} setMembers={setMembers} />
                             )
                         }
-                        <button className={styles.addMember} onClick={addMember}>
+
+                        {/* <button className={styles.addMember} onClick={addMember}>
                             + Add Member
-                        </button>
+                        </button> */}
                     </div>
-                    <div className={styles.subCont}><input type="submit" className={styles.submit} value="SUBMIT" /></div>
+                    <div className={styles.subCont}><input type="submit" className={styles.submit} value="SUBMIT" onClick={submitForm} /></div>
                 </form>
             </div>
         </div>
