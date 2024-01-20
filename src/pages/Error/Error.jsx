067af0bdddde5f6524b03487c175812f7583e770
Swarup Chanda <swarupchanda1963@gmail.com>
2024-01-20 23:18:33 +0530

    Made the error page responsive

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,5 @@
 import styles from "./Error.module.css";
-import error_txt from "../../../public/elements/404_text.svg"
+import error_txt from "/elements/404_text.svg"
 import { Link } from "react-router-dom";
 
 
