f0014dd26eb5849339ca200a5a86c16e154ff756
Priyabrat <prybrt06@gmail.com>
2024-01-21 00:55:22 +0530

    Event Page Backend Integration (#36)
    Co-authored-by: Priyabrat  Duarah <priyabrat@Priyabrats-MacBook-Air.local>

--- src/pages/EventDescription/EventDescription.jsx
+++ src/pages/EventDescription/EventDescription.jsx
@@ -1,68 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import styles from "./EventDescription.module.css";
-import robowarsImage from "/images/robowars.png";
 import backIcon from "/images/backIcon.png";
-import modulesData from "../../assets/modulesData.json";
-import { Link } from 'react-router-dom'; 
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const EventDescription = () => {
-  const [moduleData, setModuleData] = useState(null);
+  const { id } = useParams();
+  const [name, setName] = useState("");
+  const [moduleName, setModuleName] = useState("");
+  const [description, setDescription] = useState("");
+  const [posterImage, setPosterImage] = useState("");
+
+  const getEvent = async () => {
+    const url = `${import.meta.env.VITE_BASE_URL}/api/event/${id}`;
+    const response = await axios(url, {
+      headers: { Authorization: "Bearer 1000000" },
+    });
+
+    if (response.status === 200) {
+      const msg = response.data.msg;
+      setName(msg.name);
+      setModuleName(msg.module.name);
+      setDescription(msg.description);
+      setPosterImage(msg.posterImage);
+    }
+  };
 
   useEffect(() => {
-    
-    const selectedModule = modulesData.find((module) => module.moduleId === 1);
+    getEvent();
+  }, []);
 
-    if (selectedModule) {
-      setModuleData(selectedModule);
-    }
-  }, [1]);
-
-  if (!moduleData) {
-    return null; 
-  }
-
-  const { moduleHeading, description, centerImage } = moduleData;
   return (
     <div className={styles.container}>
-      
-      
-      <div className={styles.gradient} >
-      
-      <div className={styles.robowars}>
-      <Link to="">
-      <div className={styles.backIcon}><img  alt="" src={backIcon} /></div></Link>
-         <div className={styles.robowars_img}> ROBOWARS</div>
+      <div className={styles.gradient}>
+        <div className={styles.robowars}>
+          <Link to="/">
+            <div className={styles.backIcon}>
+              <img alt="" src={backIcon} />
+            </div>
+          </Link>
+          <div className={styles.robowars_img}> {name}</div>
         </div>
-    
-        <div className={styles.moduleWrapper}>
-          Module : {moduleHeading}
-        </div>
+
+        <div className={styles.moduleWrapper}>Module : {moduleName}</div>
         <div className={styles.center}>
-        <div className={styles.description}>
-        {description.map((paragraph, index) => (
-              <p key={index} className={styles.text}>{paragraph}</p>
-            ))}
+          <div className={styles.description}>
+            <p className={styles.text}>{description}</p>
+          </div>
+          <div className={styles.center_img}>
+            <img alt="robologo_demo.png" src={posterImage} />
+          </div>
         </div>
-        <div className={styles.center_img}>
-          
-            <img alt="robologo_demo.png" src={centerImage} />
-            
-        
-        </div>
-        </div>
-        <button className={styles.button}>
-          
-           Register
-           
-        </button>
-      
-      
-    
-    </div>
+        <button className={styles.button}>Register</button>
+      </div>
     </div>
   );
 };
 
 export default EventDescription;
-
-
