789cb287a9c35c086aecec2a4d61290dd1d59f77
VivekKumarSahni <viveksahni78875@gmail.com>
2024-01-21 16:57:26 +0530

    feat: add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Home, Error, Dashboard, ModulePage, EventDescription, Form } from "./pages";
 import { Routes, Route } from "react-router-dom";
-import { Navbar, Footer } from "./components";
+import { Navbar, Footer, Loading } from "./components";
 import UserContext from "./globals/authprovider";
 import "./App.css";
 
@@ -8,16 +8,17 @@ function App() {
   return (
     <>
       <UserContext>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/modules" element={<ModulePage/>} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/Loading" element={<Loading/>} />
           <Route path="/signup" element={<Form/>}/>
           <Route path="/event/:id" element={<EventDescription/>} />
           <Route path="*" element={<Error />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </UserContext>
     </>
   );
