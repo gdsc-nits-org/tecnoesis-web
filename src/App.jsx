3d448025453f04e1fa1a174c5a9f583a4ac125ae
barnilsarma <barnilsarma@gmail.com>
2024-01-21 12:23:35 +0530

    update env in registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Home, Error, Dashboard, EventDescription, ModulePage, Form } from "./pages";
+import { Home, Error, Dashboard, EventDescription, ModulePage, Registration, Form } from "./pages";
 import { Routes, Route } from "react-router-dom";
 import { Navbar, Footer } from "./components";
 import UserContext from "./globals/authprovider";
@@ -11,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/registration" element={<Registration />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/event/:id" element={<EventDescription />} />
           <Route path="*" element={<Error />} />
