05ef588ed6cfd739881a6058ea90cf096d6622df
Dhiraj <rd6999787@gmail.com>
2024-01-24 20:59:31 +0530

    changes done on page

--- src/components/Team/Card.jsx
+++ src/components/Team/Card.jsx
@@ -1,51 +1,46 @@
 import React from "react";
 import styles from "../Team/Card.module.css";
 import frame from "../../../public/images/teamPage/frame.svg";
-import member from "../../../public/images/teamPage/member.svg";
+import member from "../../../public/images/teamPage/member.webp";
 import logo from "../../../public/images/teamPage/logo.svg";
 import tecnoSid from "../../../public/images/teamPage/technoSid.svg";
 import frameBorder from "../../../public/images/teamPage/border.png";
-import linkedin from "../../../public/images/teamPage/linkedin.svg"
-import facebook from "../../../public/images/teamPage/facebook.svg"
-import github from "../../../public/images/teamPage/github.svg"
+import linkedin from "../../../public/images/teamPage/linkedin.svg";
+import facebook from "../../../public/images/teamPage/facebook.svg";
+import github from "../../../public/images/teamPage/github.svg";
 
-export const Card = () => {
+export const Card = ({name , designation , image}) => {
   return (
     <div className={styles.container}>
       <div className={styles.frame}>
         <img src={frame} alt="Image Frame" className={styles.imageFrame} />
-        <img src={logo} alt="Image Frame" className={styles.imageFrame} />
+        <img src={logo} alt="Image Frame" className={styles.logo} />
         <img
           src={frameBorder}
           alt="Image Frame"
-          className={styles.imageFrame}
-        />
-        <img
-          src={tecnoSid}
-          alt="Image Frame"
-          className={`${styles.imageFrame} ${styles.tecnoSid}`}
+          className={`${styles.imageFrame} ${styles.frame}`}
         />
 
-        <div className={styles.dp}>
-          <img src={member} alt="Image Frame" />
-          
+        <img src={image} alt="Image Frame" className={`${styles.imageFrame} ${styles.dp}`}/>
+
         <div className={styles.profile}>
           <div className={styles.prProfile}>
-            <div className={styles.name}>Name</div>
-            <div className={styles.position}>Developer</div>
+            <div className={styles.name}>{name}</div>
+            <div className={styles.position}>{designation}</div>
           </div>
           <div className={styles.SocialProfile}>
-            <div className={styles.linkedin}><img src={linkedin}/></div>
-            <div className={styles.github}><img src={github}/></div>
-            <div className={styles.facebook}><img src={facebook}/></div>
+            <div className={styles.icon}>
+              <img src={linkedin} />
+            </div>
+            <div className={styles.icon}>
+              <img src={github} />
+            </div>
+            <div className={styles.icon}>
+              <img src={facebook} />
+            </div>
           </div>
         </div>
-        </div>
-
-
-
       </div>
-
     </div>
   );
 };
--- src/pages/TeamPage/Teampage.jsx
+++ src/pages/TeamPage/Teampage.jsx
@@ -1,10 +1,207 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Teampage.module.css";
 import { Card } from "../../components/Team/Card";
+import toggleLeft from "../../../public/images/teamPage/toggle1.svg"
+import toggleRight from "../../../public/images/teamPage/toggleRight.svg"
 
 const Teampage = () => {
+  const [isSticky, setIsSticky] = useState(false);
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+  const [isSmallScreen , setIsSmallScreen ] = useState(false);
 
-  const [teams, setTeams] = useState(["TeamName" ,"TeamName" ,"TeamName" ,"TeamName" ,"TeamName" ]);
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+
+      // Adjust the offset value based on design
+      if (offset < 200) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setIsSidebarVisible(false);
+        setIsSmallScreen(true);
+      } else {
+        setIsSidebarVisible(true);
+        setIsSmallScreen(false);
+      }
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    window.addEventListener("scroll", handleScroll);
+    // Listen for window resize events
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const hadleToggle = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+  };
+
+  const [teams, setTeams] = useState([
+    {
+      id: 1,
+      name: "Team Name1",
+      members: [
+        {
+          id: 1,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 4,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Team Name2",
+      members: [
+        {
+          id: 1,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 4,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Team Name3",
+      members: [
+        {
+          id: 1,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 4,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "Team Name4",
+      members: [
+        {
+          id: 1,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 4,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+      ],
+    },
+    {
+      id: 5,
+      name: "Team Name5",
+      members: [
+        {
+          id: 1,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 2,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 3,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+        {
+          id: 4,
+          name: "Name",
+          designation: "developer",
+          image: "",
+        },
+      ],
+    },
+  ]);
+
+  const [index, setIndex] = useState(0);
 
   return (
     <div className={styles.wrapper}>
@@ -12,25 +209,51 @@ const Teampage = () => {
         <div className={styles.heading}>
           <h1>MEET THE TEAM</h1>
         </div>
+
+        <div className={styles.heading}>{teams[index].name}</div>
       </div>
-      <div className={styles.teamCards}> 
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <div className={styles.teamCards}>
+        {teams[index].members.map((member, id) => (
+          <Card
+            key={id}
+            name={member.name}
+            designation={member.designation}
+            image={member.image}
+          />
+        ))}
       </div>
 
-      <div className={styles.rightFrameParent}>
+      <div
+        className={`${isSticky ? styles.sticky : ""} ${
+          styles.rightFrameParent
+        }`}
+        style={{ display: isSidebarVisible ? "" : "none" }}
+      >
         <div className={styles.rightFrame}>
-            {
-              teams.map((teamName)=>(
-                <div className={styles.teamName} key={teamName.id}>
-                  {teamName}
-                </div>
-              ))
-            }
+          {teams.map((TeamName, id) => (
+            <div
+              key={id}
+              className={styles.teamName}
+              onClick={() => {
+                setIndex(id);
+              }}
+            >
+              {TeamName.name}
+            </div>
+          ))}
         </div>
+
       </div>
+
+      <div className={`${styles.toggle} ${isSticky ? styles.sticky : ""}`} onClick={hadleToggle}>
+          {
+            isSmallScreen && (isSidebarVisible ? (
+              <img src={toggleRight} alt="img"/>
+            ) :(
+              <img src={toggleLeft} alt="img"/>
+            )) 
+          }
+        </div>
     </div>
   );
 };
