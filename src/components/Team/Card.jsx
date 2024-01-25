9436be0cd35a18728cc4c172f36e94d89ebc853f
Swarup Chanda <swarupchanda1963@gmail.com>
2024-01-25 19:27:21 +0530

    Added the json file

--- src/components/Team/Card.jsx
+++ src/components/Team/Card.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import styles from "../Team/Card.module.css";
-import frame from "../../../public/images/teamPage/frame.svg";
-import member from "../../../public/images/teamPage/member.webp";
-import logo from "../../../public/images/teamPage/logo.svg";
-import tecnoSid from "../../../public/images/teamPage/technoSid.svg";
-import frameBorder from "../../../public/images/teamPage/border.png";
-import linkedin from "../../../public/images/teamPage/linkedin.svg";
-import facebook from "../../../public/images/teamPage/facebook.svg";
-import github from "../../../public/images/teamPage/github.svg";
+import frame from "/images/teamPage/frame.svg";
+import member from "/images/teamPage/member.webp";
+import logo from "/images/teamPage/logo.svg";
+import tecnoSid from "/images/teamPage/technoSid.svg";
+import frameBorder from "/images/teamPage/border.png";
+import linkedin from "/images/teamPage/linkedin.svg";
+import facebook from "/images/teamPage/facebook.svg";
+import github from "/images/teamPage/github.svg";
 
 export const Card = ({ name, designation, image }) => {
   return (
     <div className={styles.container}>
       <div className={styles.frame}>
         <div className={styles.frameChild}>
-          <img src={frame} alt="Image Frame" className={styles.imageFrame} />
-          <img src={logo} alt="Image Frame" className={styles.logo} />
+          <img src={frame} className={styles.imageFrame} />
+          <img src={logo} className={styles.logo} />
           <img
             src={frameBorder}
-            alt="Image Frame"
+
             className={`${styles.imageFrame} ${styles.frame}`}
           />
 
           <img
             src={image}
-            alt="Image Frame"
+
             className={`${styles.imageFrame} ${styles.dp}`}
           />
         </div>
