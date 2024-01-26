2cf998674d8e0ca9bb409cf25f9605c5b84425dd
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2024-01-26 12:41:59 +0530

    format code and upload images to cloudinary

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,7 +1,5 @@
 import styles from "./Error.module.css";
-import error_txt from "/elements/404_text.svg"
 import { Link } from "react-router-dom";
-
 
 const Error = ({ hideNavbar }) => {
   if (hideNavbar) {
@@ -10,9 +8,11 @@ const Error = ({ hideNavbar }) => {
   return (
     <div className={styles.error_container}>
       <div className={styles.err_txt}>
-        <img src={error_txt} alt="error_txt" />
-        <div className={styles.sub_txt} >We don’t know how you made it here, but</div>
-        <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
+        <img src="https://res.cloudinary.com/dnitrnzho/image/upload/v1706252971/404_text_kmdlsu.svg" alt="error_txt" />
+        <div className={styles.sub_txt}>
+          We don’t know how you made it here, but
+        </div>
+        <Link to="/" style={{ textDecoration: "none", color: "black" }}>
           <div className={styles.button_sign_content}>
             <div className={styles.btn_signin}>Lets's Get You Back Home</div>
           </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,11 @@
-import { Hero, Sponsors, About} from "../../components";
-
+import { Hero, Sponsors, About } from "../../components";
 
 const Home = () => {
   return (
     <>
-      <Hero/>
-       <About />
-      <Sponsors /> 
+      <Hero />
+      <About />
+      <Sponsors />
     </>
   );
 };
