edba83c688a61a3c43fa0af93dbb1fe6770a51fb
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2024-01-26 14:08:49 +0530

    [FIX]: Minor bugs and Optimise codebase (#56)
    * fix errors in signup form
    
    * format code and upload images to cloudinary
    
    * fix typos in teampage

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,7 +1,5 @@
 import styles from "./Error.module.css";
-// import error_txt from "/elements/404_text.svg"
 import { Link } from "react-router-dom";
-
 
 const Error = ({ hideNavbar }) => {
   if (hideNavbar) {
